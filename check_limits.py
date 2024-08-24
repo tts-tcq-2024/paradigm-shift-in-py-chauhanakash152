57b4538065bd9213ca8b54e7a40b7f92bb775b66
chauhanakash152 <chauhanakash152@gmail.com>
2024-08-24 14:14:28 +0530

    creating different functions: temp_is_ok soc_is_ok charge_rate_is_ok

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,57 @@
+def temp_is_ok(temperature):
+    """Check if the temperature is within the acceptable range.
+
+    Args:
+        temperature (float): The temperature value to check, in degrees Celsius.
+
+    Returns:
+        bool: True if the temperature is between 0 and 45 degrees Celsius,
+        inclusive; False otherwise.
+    """
+    return False if temperature < 0 or temperature > 45 else True
+
+
+def soc_is_ok(soc):
+    """Check if the state of charge (SoC) is within the acceptable range.
+
+    Args:
+        soc (float): The state of charge (SoC) percentage to check.
+
+    Returns:
+        bool: True if the SoC is between 20% and 80%, inclusive; False otherwise.
+    """
+    return False if soc < 20 or soc > 80 else True
+
+
+def charge_rate_is_ok(charge_rate):
+    """Check if the battery parameters are within their acceptable ranges.
+
+    Args:
+        temperature (float): The temperature of the battery, in degrees Celsius.
+        soc (float): The state of charge (SoC) percentage of the battery.
+        charge_rate (float): The charge rate of the battery.
+
+    Returns:
+        bool: True if all parameters (temperature, SoC, and charge rate)
+        are within their acceptable ranges; False otherwise.
+    """
+    return False if charge_rate > 0.8 else True
+
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range! first commit')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+    """Check if the battery parameters are within their acceptable ranges.
 
-  return True
+    Args:
+        temperature (float): The temperature of the battery, in degrees Celsius.
+        soc (float): The state of charge (SoC) percentage of the battery.
+        charge_rate (float): The charge rate of the battery.
 
+    Returns:
+        bool: True if all parameters (temperature, SoC, and charge rate) 
+        are within their acceptable ranges; False otherwise.
+    """
+    return temp_is_ok(temperature) and soc_is_ok(soc) and charge_rate_is_ok(charge_rate)
 
-if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+if __name__ == "__main__":
+    assert battery_is_ok(25, 70, 0.7) is True
+    assert battery_is_ok(50, 85, 0) is False
